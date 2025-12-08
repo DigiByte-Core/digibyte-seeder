23277109f9a468fb4529f5a851a7076f57289042
Bastian Driessen <bas@xobas.com>
2025-12-08 11:43:14 +1000

    Change testnet port to 12029 (testnet7).

--- protocol.h
+++ protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 12028 : 12024;
+    return testnet ? 12029 : 12024;
 }
 
 //
